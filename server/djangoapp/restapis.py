f35d7dace6952efa39c6dcd3a674b1e76021f309
KoneyWell <koneywell@hotmail.com>
2024-09-22 15:34:10 -0400

    module

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -23,10 +23,12 @@ def get_request(endpoint, **kwargs):
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
+        #print(response)
         return response.json()
     except:
         # If any error occurs
         print("Network exception occurred")
+        #print(response)
 
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
